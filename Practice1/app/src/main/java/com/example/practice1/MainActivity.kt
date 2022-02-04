004c31032d16d006986daa43a8dac27861aaf5d1
kjh <rlawjdgjs73@gmail.com>
2022-02-05 01:36:30 +0900

    lec1

--- Practice1/app/src/main/java/com/example/practice1/MainActivity.kt
+++ Practice1/app/src/main/java/com/example/practice1/MainActivity.kt
@@ -1,24 +1,37 @@
 package com.example.practice1
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.practice1.ui.theme.Practice1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Andorid", "JetpackCompose"))
+            Practice1Theme {
+                Conversation(SampleData.conversationSample)
+            }
         }
     }
 }
@@ -35,21 +48,154 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp) // 이미지 크기 설정
                 .clip(CircleShape) // 이미지 shape 설정
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape) // border의 색상과 모양 설정
         )
 
         Spacer(modifier = Modifier.width(8.dp)) // 너비 8dp짜리 빈 공간 생성
 
-        Column {
-            Text(text = msg.author)
+        var isExpand by remember { mutableStateOf(false) }
+        /*
+        state 생성하는 방법
+        val isExpand : MutableState<Boolean> = remember {
+            mutableStateOf(false)
+        }
+        val (isExpand:Boolean, setBoolean: (Boolean) -> Unit) = remember {
+            mutableStateOf(false)
+        }
+        */
+
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpand) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpand = !isExpand }) {
+            // Column과 Row는 리니어 레이아웃과 비슷하다.
+            // Box는 프레임 레이아웃과 비슷하다. (겹치기 가능)
+            // isExpand를 토글로 사용한다,
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant, // 글꼴 추가
+                style = MaterialTheme.typography.subtitle2, // 서채 추가
+            )
             Spacer(modifier = Modifier.height(4.dp)) // 글자 사이에 높이 4dp 짜리 빈 공간 생성
-            Text(text = msg.body)
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2,
+                    maxLines = if (isExpand) Int.MAX_VALUE else 1
+                )
+            }
         }
     }
 
 }
 
-@Preview
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn { // lazyColumn은 리사이클러뷰와 작동방식이 비슷한가?
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview("Light Mode")
+@Preview(
+    name = "Dark Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(msg = Message("Colleage", "Hey"))
+    Practice1Theme {
+        MessageCard(msg = Message("Colleage", "Hey"))
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    Practice1Theme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Colleague",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Colleague",
+            "List of Android versions:\n" +
+                    "Android KitKat (API 19)\n" +
+                    "Android Lollipop (API 21)\n" +
+                    "Android Marshmallow (API 23)\n" +
+                    "Android Nougat (API 24)\n" +
+                    "Android Oreo (API 26)\n" +
+                    "Android Pie (API 28)\n" +
+                    "Android 10 (API 29)\n" +
+                    "Android 11 (API 30)\n" +
+                    "Android 12 (API 31)\n"
+        ),
+        Message(
+            "Colleague",
+            "I think Kotlin is my favorite programming language.\n" +
+                    "It's so much fun!"
+        ),
+        Message(
+            "Colleague",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Colleague",
+            "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        Message(
+            "Colleague",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Colleague",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Colleague",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Colleague",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Colleague",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Colleague",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Colleague",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Colleague",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
 }
